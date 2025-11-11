c78374f10f3787c7517915026b29e38f5454c060
Frankie Alfano <q24xboss@gmail.com>
2025-11-11 01:59:17 -0500

    Set up C++ and other project files

---
+++ Assignment_4/Source/Assignment_4/APIMapCreator.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "APIMapCreator.h"
+
+// Sets default values
+AAPIMapCreator::AAPIMapCreator()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAPIMapCreator::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAPIMapCreator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
