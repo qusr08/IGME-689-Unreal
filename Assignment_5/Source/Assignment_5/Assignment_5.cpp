943d348bdadb25cec4f0299dc858aa0abe4a0479
Frankie Alfano <q24xboss@gmail.com>
2025-12-01 07:53:57 -0500

    Forgot to push before break (assignment 5)

---
+++ Assignment_5/Source/Assignment_5/Assignment_5.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Assignment_5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Assignment_5, "Assignment_5" );
